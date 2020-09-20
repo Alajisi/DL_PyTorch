d6aa995d5ee292a454efd164c24175823a4dc556
Alajisi <1875667444@qq.com>
2020-09-20 19:33:03 +0800

    准备

--- 2_prerequisite.py
+++ 2_prerequisite.py
@@ -27,5 +27,10 @@ a = torch.ones(3)
 b = 10
 print(a + b)
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Assuming that we are on a CUDA machine, this should print a CUDA device:
+
+print(device)
 
 
